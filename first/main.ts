0b001e40616ce1d24e02d4aed44a6c67aaf06732
OlafDev709 <oladavis709@gmail.com>
2025-06-30 22:24:22 -0700

    fix aLOC

--- first/main.ts
+++ first/main.ts
@@ -3,6 +3,7 @@ const password = "super-secure-pa$$word";
 
 const hash = await Bun.password.hash(password);
 // => $argon2id$v=19$m=65536,t=2,p=1$tFq+9AVr1bfPxQdh...
+console.log(hash);
 
 try {
     const isMatch = await Bun.password.verify(password, hash);
